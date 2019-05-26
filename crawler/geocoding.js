e82060a5e12a4a50c456c968a90aa4e5c7238a11
Alexandre Normand <ale.normand@icloud.com>
2019-05-26 11:37:46 +0200

    ajout variables env

--- crawler/geocoding.js
+++ crawler/geocoding.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-const apiK = "AIzaSyDKAYgVkJUMbQJ6KzkQWuXfgntS-vRGtjc";
+const apiK = process.env.GOOGLE_APIK;
 
 urlTest = 'https://maps.googleapis.com/maps/api/geocode/json?address=32+quai+d’Austerlitz,+75013,+Paris,+France&key=AIzaSyDKAYgVkJUMbQJ6KzkQWuXfgntS-vRGtjc';
 
