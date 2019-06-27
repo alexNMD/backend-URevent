d11afbbdc21e9eeb43b5f0a35230178145e02774
alexNMD <ale.normand@icloud.com>
2019-06-27 20:24:16 +0200

    ok

---
+++ test/index-spec.js
@@ -0,0 +1,56 @@
+const expect = require('chai').expect;
+const routes = require('../api/api');
+const supertest = require("supertest");
+var should = require("should");
+
+const userToken = process.env.userToken;
+const PORT = process.env.PORT;
+let url = 'http://localhost:' + PORT + '/api';
+let server = supertest.agent(url);
+
+// FORCE QUIT NODE SERVER : "killall node"
+
+describe('API test', () => {
+    it('Doit retourner tous les événements (avec token)', function(done) {
+        // Test Get All Events with token
+        server
+            .get("/events")
+            .set('Authorization', 'Bearer ' + userToken)
+            .expect("Content-type",/json/)
+            .expect(200) // This is HTTP response
+            .end(function(err,res){
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                done();
+            });
+    });
+    it('Ne doit pas retourner tous les événements (sans token)', function(done) {
+        // Test Get All Events without token
+        server
+            .get("/events")
+            .expect("Content-type",/json/)
+            .expect(401) // This is HTTP response
+            .end(function(err,res){
+                // HTTP status should be 200
+                res.status.should.equal(401);
+                done();
+            });
+    });
+    it('Doit afficher une page 404', function(done) {
+        // Test default routing (404)
+        server
+            .get("/jenesiaspas")
+            .set('Authorization', 'Bearer ' + userToken)
+            .expect("Content-type",/json/)
+            .expect(404) // This is HTTP response
+            .end(function(err,res){
+                // HTTP status should be 200
+                res.status.should.equal(404);
+                console.log(res.body);
+                done();
+            });
+    });
+
+});
+
+// TODO : Faire plus de test
